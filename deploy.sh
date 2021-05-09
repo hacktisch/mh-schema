c30f05ff83974caf5124792fecd4af836865b988
Merijn <merijn@hacktisch.com>
2021-05-09 10:52:17 +0200

    test CI

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git commit -m "$desc" && \
 git push origin master && \
 
 version=$(git log --pretty=format:'%h' -n 1)
-echo "Installing version: $version to ev-api"
-cd ../ev-api && npm i "https://hacktisch:32e194240d8bfff47319ce634d6c10f5a89a636b@github.com/hacktisch/ev-schema.git#$version"
-echo "Installing version: $version to ev-cms"
-cd ../ev-cms && npm i "https://hacktisch:32e194240d8bfff47319ce634d6c10f5a89a636b@github.com/hacktisch/ev-schema.git#$version"
+echo "Installing version: $version to mh-api"
+cd ../mh-api && npm i "https://hacktisch:32e194240d8bfff47319ce634d6c10f5a89a636b@github.com/hacktisch/mh-schema.git#$version"
+echo "Installing version: $version to mh-cms"
+cd ../mh-cms && npm i "https://hacktisch:32e194240d8bfff47319ce634d6c10f5a89a636b@github.com/hacktisch/mh-schema.git#$version"
--- timestamp.js
+++ timestamp.js
@@ -1,1 +1,1 @@
-module.exports='2021-05-07'
+module.exports='2021-05-09'
