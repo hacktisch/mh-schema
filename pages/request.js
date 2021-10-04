035d0cfa97262b525b02887f6174e96942de6ccf
Merijn <merijn@hacktisch.com>
2021-10-04 18:07:07 +0200

    thank u

--- pages/request.js
+++ pages/request.js
@@ -1,14 +1,33 @@
 module.exports = [
-    { label: "Page title", type: "text", source: "title" },
-    require("../groups/pathReference.js"),
-    
     {
+        label: "Message bar: Payment received",
+        source: "msg_received",
+        type: "text"
+    },
+    {
+        label: "Message bar: Payment still being processed",
+        source: "msg_processing",
+        type: "text"
+    },
+    { label: "Title for successful payment", type: "text", source: "title" },
+
+    {
+        label: "Page text for successful payment",
         source: "body",
-        type: "markdown",
-        label: "Page text"
+        type: "markdown"
     },
 
- 
+    {
+        label: "Title for cancelled payment",
+        type: "text",
+        source: "title_cancel"
+    },
+
+    {
+        label: "Page text for cancelled payment",
+        source: "body_cancel",
+        type: "markdown"
+    },
 
     ...require("../groups/metaTags.js")
 ];
