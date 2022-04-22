5951e5c9b9964402e2705c0281acf2908a592c07
Merijn <merijn@hacktisch.com>
2022-04-22 15:57:08 +0200

    remove key

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git push origin master && \
 
 version=$(git log --pretty=format:'%h' -n 1)
 echo "Installing version: $version to mh-api"
-cd ../mh-api && npm i "https://x-oauth-basic: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@github.com/hacktisch/mh-schema.git#$version"
+cd ../mh-api && npm i "https://github.com/hacktisch/mh-schema.git#$version"
 echo "Installing version: $version to mh-cms"
-cd ../mh-cms && npm i "https://x-oauth-basic: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@github.com/hacktisch/mh-schema.git#$version"
+cd ../mh-cms && npm i "https://github.com/hacktisch/mh-schema.git#$version"
--- timestamp.js
+++ timestamp.js
@@ -1,1 +1,1 @@
-module.exports='2022-04-21'
+module.exports='2022-04-22'
